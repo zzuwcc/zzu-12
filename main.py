2ed171e24633168c7ffdc271aa637e1d58822777
S.l <406047187@qq.com>
2022-06-09 20:56:44 +0800

    2022.6.9 登陆界面完成50%

---
+++ main.py
@@ -0,0 +1,16 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from window import register
+
+
+# 主程序入口
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = register.Ui_Form()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
